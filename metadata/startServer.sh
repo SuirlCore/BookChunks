2afd4a623f0bfed4f6d6d8710de32bde08e00d56
SuirlCore <ffrreeqquueennccyy@gmail.com>
2025-01-14 10:05:02 -0700

    update

--- metadata/startServer.sh
+++ metadata/startServer.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# Open a single Konsole window with two split terminals
-konsole --new-tab \
-    --noclose --hold -e bash -c "ngrok http --url=reliably-apt-buzzard.ngrok-free.app 80" &
-sleep 1
+# Open Konsole and create a new session
+konsole --noclose --hold -e bash -c "ngrok http --url=reliably-apt-buzzard.ngrok-free.app 80" &
+sleep 1 # Allow the first session to initialize
 
-qdbus org.kde.konsole /Sessions/1 split-session-left-right
+# Use qdbus to split the session
+qdbus org.kde.konsole /konsole/MainWindow_1 org.kde.konsole.MainWindow.splitViewLeftRight
+
+# Send the second command to the new (right) split
 qdbus org.kde.konsole /Sessions/2 sendText "sudo tail -f /var/log/mysql/mariadb.log\n"
