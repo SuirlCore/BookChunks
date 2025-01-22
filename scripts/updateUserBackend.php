812cbc91a23b994b54a2904485a094efb355acd7
SuirlCore <ffrreeqquueennccyy@gmail.com>
2025-01-22 09:20:40 -0700

    update

--- scripts/updateUserBackend.php
+++ scripts/updateUserBackend.php
@@ -18,22 +18,31 @@ if ($conn->connect_error) {
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['updateProfileAndSettings'])) {
     $userID = intval($_POST['userID']);
     $userName = $conn->real_escape_string($_POST['userName']);
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
     $realFirstName = $conn->real_escape_string($_POST['realFirstName']);
     $realLastName = $conn->real_escape_string($_POST['realLastName']);
     $email = $conn->real_escape_string($_POST['email']);
     $fontSize = $conn->real_escape_string($_POST['fontSize']);
     $fontColor = $conn->real_escape_string($_POST['fontColor']);
     $backgroundColor = $conn->real_escape_string($_POST['backgroundColor']);
-
-    // Profile update query
+    
     $updateProfileQuery = "UPDATE users SET 
         userName = '$userName', 
-        pass = '$password', 
         realFirstName = '$realFirstName', 
         realLastName = '$realLastName', 
         email = '$email' 
         WHERE userID = $userID";
+
+    // If a new password is provided, hash and update it
+    if (!empty($_POST['password'])) {
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $updateProfileQuery = "UPDATE users SET 
+            userName = '$userName', 
+            pass = '$password', 
+            realFirstName = '$realFirstName', 
+            realLastName = '$realLastName', 
+            email = '$email' 
+            WHERE userID = $userID";
+    }
 
     // Settings update query
     $updateSettingsQuery = "UPDATE users SET 
@@ -61,4 +70,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['updateProfileAndSetti
     header("Location: ../updateUser.php");
     exit();
 }
+
 ?>
