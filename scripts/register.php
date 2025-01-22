c0c6e5441cc2fd10413a1f0960129833b88278a2
SuirlCore <ffrreeqquueennccyy@gmail.com>
2025-01-22 09:45:33 -0700

    update

--- scripts/register.php
+++ scripts/register.php
@@ -2,24 +2,31 @@
 include 'pdo.php';
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-echo 'test 1<br>';
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo 'test 2<br>';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = mysqli_real_escape_string($conn, $_POST['username']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $confirmPassword = mysqli_real_escape_string($conn, $_POST['confirmPassword']);
     $firstName = mysqli_real_escape_string($conn, $_POST['firstName']);
     $lastName = mysqli_real_escape_string($conn, $_POST['lastName']);
-    echo 'test 3<br>';
+
+    // Check if passwords match
+    if ($password !== $confirmPassword) {
+        echo "Passwords do not match. Please try again.";
+        exit();
+    }
+
     // Hash the password
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-    echo 'test 4<br>';
+
     // Insert the user into the database
-    $sql = "INSERT INTO users (userName, pass, email, realFirstName, realLastName) VALUES ('$username', '$hashed_password', '$email', '$firstName', '$lastName');";
-    echo 'test 5<br>';
+    $sql = "INSERT INTO users (userName, pass, email, realFirstName, realLastName) 
+            VALUES ('$username', '$hashed_password', '$email', '$firstName', '$lastName')";
+
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully.";
         header("Location: ../login.html"); // Redirect to login page after successful registration
@@ -27,7 +34,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
-    echo 'test 6<br>';
+
     $conn->close();
 }
 ?>
