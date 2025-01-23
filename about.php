000394591d6f19ac27991ee565b382c40a1292f7
SuirlCore <ffrreeqquueennccyy@gmail.com>
2025-01-23 10:04:43 -0700

    update

--- about.php
+++ about.php
@@ -19,7 +19,6 @@ if (!isset($_SESSION['user_id'])) {
             display: flex;
             flex-direction: column;
             height: 100vh; 
-            overflow: hidden;  
             color: <?= htmlspecialchars($_SESSION['fontColor']); ?>; /* Dynamic font color */
             background-color: <?= htmlspecialchars($_SESSION['backgroundColor']); ?>; /* Dynamic background color */
     
