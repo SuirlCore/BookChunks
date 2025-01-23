4f5a78966912badf761f3268836e59d45a97b35b
SuirlCore <ffrreeqquueennccyy@gmail.com>
2025-01-23 08:26:33 -0700

    update

--- navigation.php
+++ navigation.php
@@ -18,7 +18,6 @@ body {
     padding: 0;
     display: flex;
     flex-direction: column;
-    overflow: hidden;  
     color: <?= htmlspecialchars($fontColorChoiceNav); ?>; /* Dynamic font color */
     background-color: <?= htmlspecialchars($backgroundColorChoiceNav); ?>; /* Dynamic background color */
 
