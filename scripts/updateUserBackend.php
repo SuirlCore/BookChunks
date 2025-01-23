fde85358e730eb8409bf38c283b8f7fbb4502a24
SuirlCore <ffrreeqquueennccyy@gmail.com>
2025-01-23 08:32:33 -0700

    update

--- scripts/updateUserBackend.php
+++ scripts/updateUserBackend.php
@@ -109,7 +109,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['updateProfileAndSetti
 
     // Close the statement
     $stmt->close();
-}
+
 
 
     if ($profileResult === TRUE && $settingsResult === TRUE) {
