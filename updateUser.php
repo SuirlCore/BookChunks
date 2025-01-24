4f2b958a5ea97e4bfd70fff758b652b7f5702593
SuirlCore <ffrreeqquueennccyy@gmail.com>
2025-01-24 08:30:18 -0700

    update

--- updateUser.php
+++ updateUser.php
@@ -107,6 +107,9 @@ $conn->close();
         .times {
             font-family: "Times New Roman", serif;
         }
+        .courier {
+            font-family: "Courier New", monospace;
+        }
     </style>
 </head>
 <body>
@@ -146,9 +149,9 @@ $conn->close();
             <!-- Font Select -->
             <label for="fontSelect">Select Font</label>
             <select name="fontSelect" id="fontSelect">
-                <option class="Arial" value="Arial, sans-serif" <?= $currentFontIn == "Arial, sans-serif" ? 'selected' : '' ?>>Arial</option>
+                <option class="arial" value="Arial, sans-serif" <?= $currentFontIn == "Arial, sans-serif" ? 'selected' : '' ?>>Arial</option>
                 <option class="times" value="Times New Roman, serif" <?= $currentFontIn == "Times New Roman, serif" ? 'selected' : '' ?>>Times New Roman</option>
-                <option value="Courier New, monospace" <?= $currentFontIn == "Courier New, monospace" ? 'selected' : '' ?>>Courier New</option>
+                <option class="courier" value="Courier New, monospace" <?= $currentFontIn == "Courier New, monospace" ? 'selected' : '' ?>>Courier New</option>
                 <option value="Georgia, serif" <?= $currentFontIn == "Georgia, serif" ? 'selected' : '' ?>>Georgia</option>
                 <option value="Verdana, sans-serif" <?= $currentFontIn == "Verdana, sans-serif" ? 'selected' : '' ?>>Verdana</option>
                 <option value="Tahoma, sans-serif" <?= $currentFontIn == "Tahoma, sans-serif" ? 'selected' : '' ?>>Tahoma</option>
